58a9235a10dfc4cc3f1ed7d751ddd4d462d710a0
Kunal7069 <107999879+Kunal7069@users.noreply.github.com>
2023-10-06 02:46:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,82 +1,162 @@
 document.getElementById('calculate').addEventListener('click', function () {
-    var input = document.getElementById('main-input').value;
-    
-    // Split the input by commas and trim whitespace
-    var list = input.split(',').map(item => item.trim());
+    var list = document.getElementById('main-input').value;
+    list = list.replaceAll(' ', '');
+    list = list.split(',');
   
-    // Filter out empty strings and non-numeric values
-    list = list.filter(item => item !== '' && !isNaN(item));
-  
-    // Check if the filtered list is empty
-    if (list.length === 0) {
-      document.getElementById('output').innerHTML = 'Invalid input. Please enter valid numbers separated by commas.';
-      return;
-    }
+    list = list.map((item, index) => {
+      return parseFloat(item);
+    })
   
     var operator = document.getElementById('operator').value;
   
-    switch (operator) {
-      case 'sum':
-        var sum = list.reduce((acc, val) => acc + parseFloat(val), 0);
-        document.getElementById('output').innerHTML = sum;
-        break;
-  
-      case 'average':
-        var sum = list.reduce((acc, val) => acc + parseFloat(val), 0);
-        var average = sum / list.length;
-        document.getElementById('output').innerHTML = average;
-        break;
-  
-      case 'min':
-        var min = Math.min(...list.map(parseFloat));
-        document.getElementById('output').innerHTML = min;
-        break;
-  
-      case 'max':
-        var max = Math.max(...list.map(parseFloat));
-        document.getElementById('output').innerHTML = max;
-        break;
-  
-      case 'median':
-        list.sort((a, b) => parseFloat(a) - parseFloat(b));
-        var median;
-        if (list.length % 2 === 0) {
-          median = (parseFloat(list[list.length / 2]) + parseFloat(list[list.length / 2 - 1])) / 2;
-        } else {
-          median = parseFloat(list[Math.floor(list.length / 2)]);
-        }
-        document.getElementById('output').innerHTML = median;
-        break;
-  
-      case 'mode':
-        var modeMap = {};
-        var maxCount = 0;
-        var modes = [];
-  
-        list.forEach(function (num) {
-          var parsedNum = parseFloat(num);
-          modeMap[parsedNum] = (modeMap[parsedNum] || 0) + 1;
-  
-          if (modeMap[parsedNum] > maxCount) {
-            modes = [parsedNum];
-            maxCount = modeMap[parsedNum];
-          } else if (modeMap[parsedNum] === maxCount) {
-            modes.push(parsedNum);
-          }
-        });
-  
-        document.getElementById('output').innerHTML = modes.join(', ');
-        break;
-  
-      case 'range':
-        var min = Math.min(...list.map(parseFloat));
-        var max = Math.max(...list.map(parseFloat));
-        document.getElementById('output').innerHTML = max - min;
-        break;
-  
-      default:
-        document.getElementById('output').innerHTML = 'Invalid operator';
-        break;
+    switch (operator) { 
+        case 'sum':
+            var sum = 0;
+            for (var i = 0; i < list.length; i++) {
+              sum += parseFloat(list[i]);
+            }
+            document.getElementById('output').innerHTML = sum;
+            break;
+        
+        case 'average':
+            var sum = 0;
+            for (var i = 0; i < list.length; i++) {
+              sum += parseFloat(list[i]);
+            }
+            document.getElementById('output').innerHTML = sum / list.length;
+            break;
+        
+        case 'min':
+            var min = list[0];
+            for (var i = 0; i < list.length; i++) {
+              if (list[i] < min) {
+                min = list[i];
+              }
+            }
+            document.getElementById('output').innerHTML = min;
+            break;
+        
+        case 'max':
+            var max = list[0];
+            for (var i = 0; i < list.length; i++) {
+              if (list[i] > max) {
+                max = list[i];
+              }
+            }
+            document.getElementById('output').innerHTML = max;
+            break;
+        
+        case 'multiplication':
+            var ans = 1;
+            for (var i = 0; i < list.length; i++) {
+            
+                ans *= list[i];
+              
+            }
+            document.getElementById('output').innerHTML = ans;
+            break;
+        
+        case 'lcm':
+            function findGCD(a, b) {
+                if (b === 0) {
+                    return a;
+                } else {
+                    return findGCD(b, a % b);
+                }
+            }
+            let lcm = list[0];
+            for (let i = 1; i < list.length; i++) {
+                const currentNumber = list[i];
+                const gcd = findGCD(lcm, currentNumber);
+                lcm = (lcm * currentNumber) / gcd;
+            }
+            document.getElementById('output').innerHTML = lcm;
+            break;
+        
+        case 'hcf':
+            function findGCD(a, b) {
+                if (b === 0) {
+                  return a;
+                } else {
+                  return findGCD(b, a % b);
+                }
+            }
+            let gcd = list[0];
+            for (let i = 1; i < list.length; i++) {
+                const currentNumber = list[i];
+                gcd = findGCD(gcd, currentNumber);
+            }
+            document.getElementById('output').innerHTML = gcd;
+            break;
+                
+        case 'median':
+            // sort list
+            for (var i = 0; i < list.length; i++) {
+                for (var j = 0; j < list.length; j++) {
+                if (list[i] < list[j]) {
+                    var temp = list[i];
+                    list[i] = list[j];
+                    list[j] = temp;
+                }
+                }
+            }
+            // get median
+            var median = 0;
+            if (list.length % 2 == 0) {
+                median = (list[list.length / 2] + list[(list.length / 2) - 1]) / 2;
+            } else {
+                median = list[Math.floor(list.length / 2)];
+            }
+            document.getElementById('output').innerHTML = median;
+            break;
+        
+        case 'mode':
+        // sort list
+            for (var i = 0; i < list.length; i++) {
+                for (var j = 0; j < list.length; j++) {
+                    if (list[i] < list[j]) {
+                        var temp = list[i];
+                        list[i] = list[j];
+                        list[j] = temp;
+                    }
+                }
+            }
+        // get mode
+            var mode = 0;
+            var count = 0;
+            var maxCount = 0;
+            for (var i = 0; i < list.length; i++) {
+                if (list[i] == list[i + 1]) {
+                    count++;
+                } else {
+                    count = 0;
+                }
+                if (count > maxCount) {
+                    maxCount = count;
+                    mode = list[i];
+                }
+            }
+            document.getElementById('output').innerHTML = mode;
+            break;
+           
+        case 'range':
+            var min = list[0];
+            var max = list[0];
+            for (var i = 0; i < list.length; i++) {
+                if (list[i] < min) {
+                min = list[i];
+                }
+                if (list[i] > max) {
+                max = list[i];
+                }
+            }
+            document.getElementById('output').innerHTML = max - min;
+            break;
+        
+        default:
+            document.getElementById('output').innerHTML = 'Invalid operator';
+            break;
+        
     }
-  });
-  
+    
+  })
